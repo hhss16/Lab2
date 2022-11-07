d973daefb1a10c39466914417ca303f5819e89f0
MyMac <MyMac@MyMac.local>
2022-11-07 17:51:29 +0600

    BookView with full support for GET and POST

--- BookListDRF/views.py
+++ BookListDRF/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book
+from .seriazlizers import BookSerializer
+from rest_framework import generics
+
+class BookView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
